3e34c96a435a923aba81c64609cac9be92003659
Jorge Salazar <j-salazarp@hotmail.com>
2021-05-30 12:40:35 +0200

    test mockeando una acción del store

---
+++ src/store/actions2.test.js
@@ -0,0 +1,28 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { loginAction } from './actions';
+import { AUTH_LOGIN_REQUEST, AUTH_LOGIN_SUCCESS } from './types';
+
+
+const middleware = [thunk];
+const mockStore = configureStore(middleware);
+
+describe('loginAction', () => {
+  describe('when login api resolves', () => {
+    const credentials = 'credentials';
+    const isChecked = 'isChecked';
+    const login = jest.fn().mockResolvedValue();
+
+    test('should dispatch an AUTH_LOGIN_SUCCESS action', async () => {
+      const store = mockStore([]);
+      await store.dispatch(loginAction(credentials,isChecked));
+      const actions = store.getActions();
+      expect(actions).toEqual([
+        { type: AUTH_LOGIN_REQUEST },
+        { type: AUTH_LOGIN_SUCCESS },
+      ]);
+      expect(login).toBeCalledWith(credentials,isChecked);
+    });
+  });
+});
